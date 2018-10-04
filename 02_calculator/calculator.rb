d619e035ec60aa16a53d0d8745e555e4248453e4
Shu Jenkins <shujenkins@gmail.com>
2018-10-04 19:32:39 +0000

    Initial Commit

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,42 @@
 #write your code here
+def add(x,y)
+  x + y
+end
+
+def subtract(x,y)
+  x - y
+end
+
+def sum(a)
+  s = 0
+  a.each do |x|
+    s += x
+  end
+  s
+end
+
+=begin
+def multiply(a=3,b=7)
+   a * b
+end
+
+def product(a)
+  p = 1
+  a.each do |x|
+    p *= x
+  end
+  p
+end
+
+def power(x,y)
+  x**y
+end
+
+def fact(n)
+  f = 1
+  (2..n).each do |k|
+    f *= k
+  end
+  f
+end
+=end
